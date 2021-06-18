0f3abada21566f9fe753c76d99c4e3e4c1ec99a7
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-06-18 22:01:31 +0530

    📦 Update exisiting packages

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Opps! This page does not exist | aniruddha.net</title>
+      </Head>
+      <div>
+        <p>This page does not exist</p>
+      </div>
+    </>
+  );
+}

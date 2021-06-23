26d7f086e28a8504ac8affab9421d78cee7ae3f9
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-06-23 21:43:24 +0530

    :sparkles: Add temporary landing page

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Aniruddha Chatterjee - Software Developer</title>
+          <script
+            src="https://kit.fontawesome.com/b6373a4a93.js"
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Index() {
   return (
@@ -7,7 +9,37 @@ export default function Index() {
       <Head>
         <title>Aniruddha Chatterjee - Software Developer</title>
       </Head>
-      <p className="mx-auto text-xl">Aniruddha Chatterjee</p>
+      <img
+        src="/logo.png"
+        alt="Picture of the author"
+        className="block mx-auto w-32 my-10"
+      />
+      <p className="mx-auto text-3xl md:text-5xl font-bold text-center">
+        Aniruddha Chatterjee
+      </p>
+      <p className="w-full md:w-1/2 px-5 mx-auto text-xl text-center mt-5">
+        Hello! I see that you have found my portfolio. This one is still under
+        construction, but you can definitely check my LinkedIn an GitHub
+        profiles to get a basic idea about me.
+      </p>
+      <p
+        className="text-center text-xl mt-20 cursor-pointer"
+        title="Aniruddha on GitHub"
+        onClick={() => {
+          window.open("https://github.com/ruddha2001");
+        }}
+      >
+        <FontAwesomeIcon icon={faGithub} /> ruddha2001
+      </p>
+      <p
+        className="text-center text-xl mt-5 cursor-pointer"
+        title="Aniruddha on LinkedIn"
+        onClick={() => {
+          window.open("https://www.linkedin.com/in/ruddha2001");
+        }}
+      >
+        <FontAwesomeIcon icon={faLinkedin} /> ruddha2001
+      </p>
     </>
   );
 }
